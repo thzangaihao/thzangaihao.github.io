885ea7a8f2f76f3b8d84278aa3f38e87b8e75a09
Haizhao <thzangaihao@outlook.com>
2026-01-20 20:31:16 +0800

    AF3

--- js/article_data.js
+++ js/article_data.js
@@ -1,12 +1,12 @@
 // 自动生成的文件，请勿手动修改
-// 生成时间: 2026/01/19_22:4051
-// 文章总数: 6
+// 生成时间: 2026/01/20_20:2324
+// 文章总数: 8
 
 window.ARTICLE_DATABASE = [
     {
         "title": "01. AlphaFold3 在线版使用说明",
         "collection": "alphafold",
-        "date": "2026-01-20",
+        "date": "2026-01-19",
         "summary": "通过AlphaFold在线服务器进行简单任务",
         "path": "articles/dry_lab/alphafold/01. AlphaFold3 在线版使用说明/01. AlphaFold3 在线版使用说明.html"
     },
@@ -18,11 +18,18 @@ window.ARTICLE_DATABASE = [
         "path": "articles/dry_lab/docking/01. AutoDock Vina 基本使用/01. AutoDock Vina 基本使用.html"
     },
     {
-        "title": "01. GWAS：全基因组关联分析",
-        "collection": "data",
+        "title": "01. Pymol常用指令汇总",
+        "collection": "pymol",
+        "date": "2026-01-20",
+        "summary": "如何使你的图变得专业好看？",
+        "path": "articles/dry_lab/pymol/01. Pymol常用指令汇总/01. Pymol常用指令汇总.html"
+    },
+    {
+        "title": "01. 全基因组关联分析基础原理",
+        "collection": "gwas",
         "date": "2025-11-28",
-        "summary": "如何通过你的基因来判断你是否喜欢吃香菜？",
-        "path": "articles/dry_lab/data/01. GWAS/GWAS.html"
+        "summary": "列联分析、独立性检验与卡方检验",
+        "path": "articles/dry_lab/gwas/01. 全基因组关联分析基础原理/01. 全基因组关联分析基础原理.html"
     },
     {
         "title": "01. 转录组分析系列：富集分析",
@@ -44,5 +51,12 @@ window.ARTICLE_DATABASE = [
         "date": "2026-01-18",
         "summary": "通过 Python 脚本在 Linux 实现大规模对接任务",
         "path": "articles/dry_lab/docking/02. AutoDock Vina 自动化/02. AutoDock Vina 自动化.html"
+    },
+    {
+        "title": "03. 输出数据",
+        "collection": "alphafold",
+        "date": "2026-01-20",
+        "summary": "PAE 矩阵、plDDT、pTM 与 ipTM 得分",
+        "path": "articles/dry_lab/alphafold/03. 输出数据/03. 输出数据.html"
     }
 ];
